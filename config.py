a79c133c0b621eee3069a89f24e98848ed4e896b
DARKY-LAB <nk1010057@gmail.com>
2024-12-28 15:40:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7385295244"))
 #Port
 PORT = os.environ.get("PORT", "8020")
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb://nitinkumardhundhara:DARKXSIDE78@cluster0.wdive.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nitinkumardhundhara:DARKXSIDE78@cluster0.wdive.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "MovieCollab")
 
 #force sub channel id, if you want enable force sub
