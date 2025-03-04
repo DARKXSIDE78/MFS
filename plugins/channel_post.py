11dc8c5409b863dddd83dc47ac00113bdd273519
DARKXSIDE78 <nk1010057@gmail.com>
2025-03-04 05:09:25 -0500

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -4,10 +4,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, OWNER_ID
 from helper_func import encode
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','restart','id']))
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.user(OWNER_ID) & ~filters.command(['start','users','broadcast','batch','genlink','stats','restart','id']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Pʟᴇᴀsᴇ Wᴀɪᴛ...!", quote = True)
     try:
